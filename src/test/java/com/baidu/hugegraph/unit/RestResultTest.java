fb02511e78b1f7dfc29b46ec4c13fbc19996b51b
liningrui <liningrui@baidu.com>
2019-05-29 10:24:56 +0800

    tiny improve
    Change-Id: Id82434ce3f148ac2f44db9a29ed051c9f837a04e

--- src/test/java/com/baidu/hugegraph/unit/RestResultTest.java
+++ src/test/java/com/baidu/hugegraph/unit/RestResultTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.baidu.hugegraph.unit;
 
 import java.util.ArrayList;
@@ -31,7 +50,6 @@ import com.baidu.hugegraph.structure.schema.PropertyKey;
 import com.baidu.hugegraph.structure.schema.VertexLabel;
 import com.baidu.hugegraph.testutil.Assert;
 import com.baidu.hugegraph.testutil.Utils;
-import com.baidu.hugegraph.testutil.Whitebox;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -636,7 +654,7 @@ public class RestResultTest extends BaseUnitTest {
         Assert.assertNull(restResult.headers());
 
         Response response = restResult.readObject(Response.class);
-        Whitebox.setInternalState(response.result(), "graphManager", graph());
+        response.graphManager(graph());
         Assert.assertEquals("b0fd8ead-333f-43ac-97b0-4d78784726ae",
                             response.requestId());
         Assert.assertEquals(200, response.status().code());
@@ -724,7 +742,7 @@ public class RestResultTest extends BaseUnitTest {
         Assert.assertNull(restResult.headers());
 
         Response response = restResult.readObject(Response.class);
-        Whitebox.setInternalState(response.result(), "graphManager", graph());
+        response.graphManager(graph());
         Assert.assertEquals(200, response.status().code());
 
         Edge created = new Edge("created");
@@ -812,7 +830,7 @@ public class RestResultTest extends BaseUnitTest {
         Assert.assertNull(restResult.headers());
 
         Response response = restResult.readObject(Response.class);
-        Whitebox.setInternalState(response.result(), "graphManager", graph());
+        response.graphManager(graph());
         Assert.assertEquals(200, response.status().code());
 
         Iterator<Result> results = response.result().iterator();
@@ -870,7 +888,7 @@ public class RestResultTest extends BaseUnitTest {
         Assert.assertNull(restResult.headers());
 
         Response response = restResult.readObject(Response.class);
-        Whitebox.setInternalState(response.result(), "graphManager", graph());
+        response.graphManager(graph());
         Assert.assertEquals(200, response.status().code());
 
         Iterator<Result> results = response.result().iterator();
@@ -915,7 +933,7 @@ public class RestResultTest extends BaseUnitTest {
         Assert.assertNull(restResult.headers());
 
         Response response = restResult.readObject(Response.class);
-        Whitebox.setInternalState(response.result(), "graphManager", graph());
+        response.graphManager(graph());
         Assert.assertEquals(200, response.status().code());
 
         Iterator<Result> results = response.result().iterator();
@@ -975,7 +993,7 @@ public class RestResultTest extends BaseUnitTest {
         Assert.assertNull(restResult.headers());
 
         Response response = restResult.readObject(Response.class);
-        Whitebox.setInternalState(response.result(), "graphManager", graph());
+        response.graphManager(graph());
         Assert.assertEquals(200, response.status().code());
 
         Iterator<Result> results = response.result().iterator();

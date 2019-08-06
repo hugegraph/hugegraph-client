a7636aa1748287f18505179f1069973834a638f6
zhangyi51 <zhangyi51@baidu.com>
2019-08-06 20:21:03 +0800

    add assertThrow exception message assert
    Change-Id: I8e8c0e950683a2eee29b7642a808add281be2c6c

--- src/test/java/com/baidu/hugegraph/api/IndexLabelApiTest.java
+++ src/test/java/com/baidu/hugegraph/api/IndexLabelApiTest.java
@@ -133,46 +133,66 @@ public class IndexLabelApiTest extends BaseApiTest {
 
     @Test
     public void testCreateSecondaryIndexOnMultiPropertiesOverrideExist() {
+        String personByCity = "personByCity";
+        String personByCityAndAge = "personByCityAndAge";
         schema().indexLabel("personByCity")
                 .onV("person")
                 .by("city")
                 .secondary()
                 .create();
-        indexLabelAPI.get("personByCity");
+        indexLabelAPI.get(personByCity);
         Assert.assertThrows(ServerException.class, () -> {
-            indexLabelAPI.get("personByCityAndAge");
+            indexLabelAPI.get(personByCityAndAge);
+        }, e -> {
+            String expect = String.format("index label with name '%s' does " +
+                                          "not exist", personByCityAndAge);
+            Assert.assertTrue(e.toString(), e.getMessage().contains(expect));
         });
-        schema().indexLabel("personByCityAndAge")
+        schema().indexLabel(personByCityAndAge)
                 .onV("person")
                 .by("city", "age")
                 .secondary()
                 .create();
         Assert.assertThrows(ServerException.class, () -> {
-            indexLabelAPI.get("personByCity");
+            indexLabelAPI.get(personByCity);
+        }, e -> {
+            String expect = String.format("index label with name '%s' does " +
+                                          "not exist", personByCity);
+            Assert.assertTrue(e.toString(), e.getMessage().contains(expect));
         });
-        indexLabelAPI.get("personByCityAndAge");
+        indexLabelAPI.get(personByCityAndAge);
     }
 
     @Test
     public void testCreateShardIndexOnMultiPropertiesOverrideExist() {
-        schema().indexLabel("personByCity")
+        String personByCity = "personByCity";
+        String personByCityAndAge = "personByCityAndAge";
+        schema().indexLabel(personByCity)
                 .onV("person")
                 .by("city")
                 .secondary()
                 .create();
-        indexLabelAPI.get("personByCity");
+        indexLabelAPI.get(personByCity);
         Assert.assertThrows(ServerException.class, () -> {
-            indexLabelAPI.get("personByCityAndAge");
+            indexLabelAPI.get(personByCityAndAge);
+        }, e -> {
+            String expect = String.format("index label with name '%s' does " +
+                                          "not exist", personByCityAndAge);
+            Assert.assertTrue(e.toString(), e.getMessage().contains(expect));
         });
-        schema().indexLabel("personByCityAndAge")
+        schema().indexLabel(personByCityAndAge)
                 .onV("person")
                 .by("city", "age")
                 .shard()
                 .create();
         Assert.assertThrows(ServerException.class, () -> {
-            indexLabelAPI.get("personByCity");
+            indexLabelAPI.get(personByCity);
+        }, e -> {
+            String expect = String.format("index label with name '%s' does " +
+                                          "not exist", personByCity);
+            Assert.assertTrue(e.toString(), e.getMessage().contains(expect));
         });
-        indexLabelAPI.get("personByCityAndAge");
+        indexLabelAPI.get(personByCityAndAge);
     }
 
     @Test
@@ -235,13 +255,18 @@ public class IndexLabelApiTest extends BaseApiTest {
 
     @Test
     public void testDelete() {
-        indexLabelAPI.create(fillIndexLabel.apply("personByAge"));
+        String name = "personByAge";
+        indexLabelAPI.create(fillIndexLabel.apply(name));
 
-        long taskId = indexLabelAPI.delete("personByAge");
+        long taskId = indexLabelAPI.delete(name);
         waitUntilTaskCompleted(taskId);
 
         Assert.assertThrows(ServerException.class, () -> {
-            indexLabelAPI.get("personByAge");
+            indexLabelAPI.get(name);
+        }, e -> {
+            String expect = String.format("index label with name '%s' does " +
+                                          "not exist", name);
+            Assert.assertTrue(e.toString(), e.getMessage().contains(expect));
         });
     }
 

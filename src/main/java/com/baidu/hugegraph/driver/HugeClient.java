b7d0ee2010003394dd75ea3d9edc886cdda7af3b
guoshoujing <guoshoujing@baidu.com>
2021-06-24 14:12:37 +0800

    update hugegraph server version

--- src/main/java/com/baidu/hugegraph/driver/HugeClient.java
+++ src/main/java/com/baidu/hugegraph/driver/HugeClient.java
@@ -97,7 +97,7 @@ public class HugeClient implements Closeable {
     private void checkServerApiVersion() {
         VersionUtil.Version apiVersion = VersionUtil.Version.of(
                                          this.version.getApiVersion());
-        VersionUtil.check(apiVersion, "0.38", "0.63",
+        VersionUtil.check(apiVersion, "0.38", "0.64",
                           "hugegraph-api in server");
         this.client.apiVersion(apiVersion);
     }
